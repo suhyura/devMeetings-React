65a8c5c9f259a492de61d2674343348a263e777a
YuriyThinkpad <suhyura@gmail.com>
2018-02-10 14:39:26 +0100

    After Exorcist comes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import React, {Component} from 'react';
 import Contacts from './Contacts'
 
 class App extends Component {
-  constructor(props)
-  {
-      super(props)
-  }
+    constructor(props){
+        super(props)
+        this.state = {
+            filter : "",
+            contacts: props.data.contacts
+        }
+    }
+   
+
   onTextboxChange = (event) =>
   {
-
+    this.setState({filter : event.target.value}, () => {
+        this.state.filter === "" ? this.state.contacts : this.setState({
+            contacts: this.props.data.contacts.filter( (contact) => contact.name.includes(this.state.filter))
+        }, () => {
+            console.log(this.state.contacts);
+        })
+    });
+    
   }
+  getFilteredContacts =() => {
+    return this.state.filter === "" ? this.state.contacts : this.state.contacts.filter( (contact) => contact.name.includes(this.state.filter) )
+  }
+ 
     render() {
-        const {contacts} = this.props.data
-      return (
-          <div>
-            <input type="text" id="inputArea" onChange ={this.onTextboxChange}/>
-            <Contacts contacts = {contacts} />
-        </div>
-      );
+        return (
+            <div>
+                <input type="text" id="inputArea" onChange ={this.onTextboxChange}/>
+                <Contacts contacts = {this.state.contacts} />
+            </div>
+        );
     }
   }
 
